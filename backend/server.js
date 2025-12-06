bd90afc81f596cc46b0391407554318f049657bd
oralrobinson21 <oralrobinson21@outlook.com>
2025-12-06 12:22:22 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,7 @@ require('dotenv').config();
 const { sendTestEmail } = require('./lib/resend');
 
 const app = express();
-const PORT = process.env.BACKEND_PORT || 5001;
-
+const PORT = process.env.PORT || 5001;
 app.use(cors());
 app.use(express.json());
 
